e83a095bdfa04086e66eee8e5fa5e0015f4965ba
wsh <1252136236@qq.com>
2021-01-27 10:38:14 +0800

    add: 全局跨域

---
+++ src/main/java/com/xingzhi/xingzhiblog/common/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.xingzhi.xingzhiblog.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @program: xingzhiblog
+ * @description: 全局跨域配置
+ * @author: 行之
+ * @create: 2021-01-24 21:32
+ **/
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
